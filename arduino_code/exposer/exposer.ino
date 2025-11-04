8803026c0275612870b6e84d5e5949dce87ad1a4
kauzerei <github@kauzerei.de>
2025-11-04 22:24:50 +0100

    screen was upside down

--- arduino_code/exposer/exposer.ino
+++ arduino_code/exposer/exposer.ino
@@ -17,9 +17,12 @@
 #include <TM1637Display.h> //https://github.com/avishorp/TM1637
 
 const char led_out=7;
-const char button_up=6;
-const char button_mid=5;
-const char button_down=4;
+//const char button_up=6;
+//const char button_mid=5;
+//const char button_down=4;
+const char button=6;
+const char encoder_b=5;
+const char encoder_a=4;
 const char display_clk=2;
 const char display_dio=3;
 byte ei=10;
@@ -32,9 +35,9 @@ TM1637Display display(display_clk, display_dio);
 
 void setup() {
   pinMode(led_out,OUTPUT);
-  pinMode(button_up,INPUT_PULLUP);
-  pinMode(button_down,INPUT_PULLUP);
-  pinMode(button_mid,INPUT_PULLUP);
+  pinMode(encoder_a,INPUT_PULLUP);
+  pinMode(encoder_b,INPUT_PULLUP);
+  pinMode(button,INPUT_PULLUP);
   digitalWrite(led_out,LOW);
   display.setBrightness(0x0f);
 }
@@ -45,11 +48,13 @@ void showtime(byte ei) {
   else display.showNumberDecEx(2<<(9-ei),0b11100000);
   }
 
+
+
 void loop() {
   showtime(ei);
-  if (digitalRead(button_up)==LOW) {ei=ei+1; delay(100);}
-  if (digitalRead(button_down)==LOW) {ei=ei-1; delay(100);}
-  if (digitalRead(button_mid)==LOW) {
+//  if (digitalRead(button_up)==LOW) {ei=ei+1; delay(100);}
+//  if (digitalRead(button_down)==LOW) {ei=ei-1; delay(100);}
+  if (digitalRead(button)==LOW) {
     time=millis();
     digitalWrite(led_out,HIGH);
     while (millis()-time<pow(2,ei-10)*1000)
